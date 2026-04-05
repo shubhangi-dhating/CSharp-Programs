4b6606b9ce5d33805e9654eea1f84705f2cf4302
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 12:16:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class SumNatural
+{
+    public static void Main(string[] args)
+    {
+        int n, sum = 0;
+
+        Console.Write("Enter a number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+
+        Console.WriteLine("Sum of natural numbers = " + sum);
+    }
+}
