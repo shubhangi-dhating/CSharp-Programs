8916a09d6f813153cb059cf6f95c2a9b2c77bc87
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 20:32:22 +0530

    Add files via upload

---
+++ Replace Substring to String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class ReplaceSubstring
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a String       : ");
+        string str = Console.ReadLine();
+
+        Console.Write("Enter word to Find   : ");
+        string find = Console.ReadLine();
+
+        Console.Write("Enter word to Replace: ");
+        string replace = Console.ReadLine();
+
+        // Built-in Replace
+        string result = str.Replace(find, replace);
+
+        Console.WriteLine("\n--- Result ---");
+        Console.WriteLine("Original String  : " + str);
+        Console.WriteLine("Modified String  : " + result);
+    }
+}
