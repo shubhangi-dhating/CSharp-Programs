39dd653551ebbdaaa0da12cb9a7de27efa58c472
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 12:28:49 +0530

    Add files via upload

---
+++ DaysConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class DaysConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter days: ");
+        int d = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Years: {d / 365}, Months: {d % 365 / 30}, Days: {d % 365 % 30}");
+    }
+}
