4aa196d52d8e216180d684e463c4f93aaf5ef12a
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 12:20:03 +0530

    Add files via upload

---
+++ Leap Year.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+class LeapYear
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            Console.WriteLine($"{year} is a Leap Year.");
+        else
+            Console.WriteLine($"{year} is NOT a Leap Year.");
+    }
+}
