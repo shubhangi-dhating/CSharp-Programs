5af2e98c3dbc2f6b422c3a10d04aa1352f2d2c9b
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 20:01:10 +0530

    Add files via upload

---
+++ Reverse of String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ReverseString
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a String: ");
+        string str = Console.ReadLine();
+
+        // Convert to char array and reverse
+        char[] chars = str.ToCharArray();
+        Array.Reverse(chars);
+
+        string reversed = new string(chars);
+
+        Console.WriteLine("Original String : " + str);
+        Console.WriteLine("Reversed String : " + reversed);
+    }
+}
