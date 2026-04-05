83f919f09b6cccd4dadb1cd0d7a547d6a85be107
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 14:30:32 +0530

    Add files via upload

---
+++ ArrayElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ArrayDemo
+{
+    static void Main()
+    {
+        Console.Write("Enter size of array: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        // Read
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Enter element {i + 1}: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Display
+        Console.WriteLine("\nArray Elements:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+    }
+}
