090348858fd5f9fa5b587ae454f045a723e25d7e
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 20:14:18 +0530

    Add files via upload

---
+++ String Pallindrom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class PalindromeString
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a String: ");
+        string str = Console.ReadLine().ToLower();
+
+        // Reverse the string
+        char[] chars = str.ToCharArray();
+        Array.Reverse(chars);
+        string reversed = new string(chars);
+
+        Console.WriteLine("\nOriginal String : " + str);
+        Console.WriteLine("Reversed String : " + reversed);
+
+        if (str == reversed)
+            Console.WriteLine("Result          : String is  PALINDROME!");
+        else
+            Console.WriteLine("Result          : ❌ NOT a Palindrome!");
+    }
+}
