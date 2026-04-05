e2d7aebb09a9f175573e71fac365f65d21495045
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 19:04:17 +0530

    Add files via upload

---
+++ Reverse Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class ReverseArray
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number of elements: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Element [" + (i + 1) + "]: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("\nOriginal Array: ");
+        foreach (int x in arr)
+            Console.Write(x + " ");
+
+        // Built-in Reverse
+        Array.Reverse(arr);
+
+        Console.Write("\nReversed Array: ");
+        foreach (int x in arr)
+            Console.Write(x + " ");
+
+        Console.WriteLine();
+    }
+}
