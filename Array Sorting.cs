312dcff306e8ec6f353d151654ba5a21529d34a1
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 18:56:52 +0530

    Add files via upload

---
+++ Array Sorting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class SortArray
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number of elements: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Element [" + (i + 1) + "]: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Built-in Sort (Ascending)
+        Array.Sort(arr);
+
+        Console.Write("\nSorted (Ascending):  ");
+        foreach (int x in arr)
+            Console.Write(x + " ");
+
+        // Built-in Reverse (Descending)
+        Array.Reverse(arr);
+
+        Console.Write("\nSorted (Descending): ");
+        foreach (int x in arr)
+            Console.Write(x + " ");
+
+        Console.WriteLine();
+    }
+}
