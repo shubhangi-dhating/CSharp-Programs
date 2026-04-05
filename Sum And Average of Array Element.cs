679ada69c14b13f98abc5a83a7d043c27657f3a4
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-05 18:46:11 +0530

    Add files via upload

---
+++ Sum And Average of Array Element.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class SumAndAverage
+{
+    static void Main(string[] args)
+    {
+        // Input array size
+        Console.Write("Enter number of elements: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        // Input array elements
+        Console.WriteLine("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Element [" + (i + 1) + "]: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Calculate Sum
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+
+        // Calculate Average
+        double average = (double)sum / n;
+
+        // Output
+        Console.WriteLine("\n--- Result ---");
+        Console.Write("Array Elements: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Sum     = " + sum);
+        Console.WriteLine("Average = " + average);
+    }
+}
