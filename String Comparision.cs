e44559b89885afd21bbf3a4aeb86c588f19c87ea
shubhangi-dhating <shubhangidhating@gmail.com>
2026-04-07 10:53:15 +0530

    Add files via upload

---
+++ String Comparision.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first string: ");
+        string str1 = Console.ReadLine();
+
+        Console.Write("Enter second string: ");
+        string str2 = Console.ReadLine();
+
+        if (str1 == str2)
+        {
+            Console.WriteLine("Strings are equal");
+        }
+        else
+        {
+            Console.WriteLine("Strings are not equal");
+        }
+    }
+}
